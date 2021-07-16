8964c2dd688417eb6f418ade04f63b9585aa90ab
Matt Anderson <manderson0182@gmail.com>
2021-07-16 12:55:12 -0600

    added a working login component

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from '../../services/authentication.service';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -10,19 +9,12 @@ import { Router } from '@angular/router';
 export class LoginComponent implements OnInit {
   username = '';
   password = '';
-  invalidLogin = false;
 
-  constructor(
-    private router: Router,
-    private loginservice: AuthenticationService
-  ) {}
+  constructor(private loginservice: AuthenticationService) {}
 
   ngOnInit() {}
 
   login() {
-    if (this.loginservice.authenticate(this.username, this.password)) {
-      this.router.navigate(['user']);
-      this.invalidLogin = false;
-    } else this.invalidLogin = true;
+    this.loginservice.authenticate(this.username, this.password);
   }
 }
